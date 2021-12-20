a32abe0b10ff1de816fbd55c727944f5472bf4c8
BaturKahraman <baturhan.kahraman@sigun.com.tr>
2021-12-20 11:42:37 +0300

    Deleting redundant files & adding settings files

---
+++ Baturhan.Logger/Settings/Setting.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MrLogger.Settings
+{
+    public class Setting:ISetting
+    {
+    }
+}
---
+++ Baturhan.Logger/Utility/Enums/LogLevels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MrLogger.Utility.Enums
+{
+    public enum LogLevels
+    {
+        Info,
+        Debug,
+        Warning,
+        Error,
+        Fatal
+    }
+}
